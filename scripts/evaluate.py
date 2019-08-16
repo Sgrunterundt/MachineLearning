404bb0300b94dd9a706c061714d5d84b58abd40c
LAPTOP-6FT9I34B\Kasper <kefeld@gmail.com>
2019-08-16 11:06:00 +0200

    Made evaluate.py more user friendly. Provides instruction to include image path if none is included instead of returning error.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -4,16 +4,19 @@ import torchnet
 from matplotlib.image import imread
 import numpy as np
 
-img = imread(sys.argv[1])
-img = 1 - img
+if len(sys.argv) < 2:
+    print("Please provide path to the image you want to read as an argument. Eg.: \"python evaluate.py ..\\data\\test.png\"")
+else:
 
-im = torch.from_numpy(np.reshape(img[:,:,1], (1,1,28,28)))
+    img = imread(sys.argv[1])
+    img = 1 - img
 
-
-net = torch.load('..\data\pretrained.pth')
-
-a = net.feedforward(im).max(1)[1].data.tolist()[0]
-
+    im = torch.from_numpy(np.reshape(img[:,:,1], (1,1,28,28)))
 
 
-print("Hmm, det ligner et {0}-tal".format(a))
+    net = torch.load('..\data\pretrained.pth')
+
+    a = net.feedforward(im).max(1)[1].data.tolist()[0]
+
+    if a==0: print("Hmm, det ligner et 0.")
+    else: print("Hmm, det ligner et {0}-tal".format(a))
